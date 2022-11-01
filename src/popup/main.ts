eff37c4167f4986fd69d54b3fd79fbc17c9a8492
Ivy <aith.mailbox@gmail.com>
2022-11-01 13:41:17 -0700

    fix: remove unresolved import

--- src/popup/main.ts
+++ src/popup/main.ts
@@ -1,10 +1,8 @@
 import { createApp } from 'vue'
 import PortalVue from 'portal-vue'
 import VueMultiselect from 'vue-multiselect'
-import ResizeTextarea from 'resize-textarea-vue3'
 import App from './Popup.vue'
 import '../styles'
-import TextAreaResize from '~/components/TextAreaResize.vue'
 
 const app = createApp(App)
 app.use(PortalVue)
