4bf1255c21366ce98377ee2ebab9fcd698b0f469
Ivy <aith.mailbox@gmail.com>
2022-11-02 21:43:58 -0700

    feat: fix storage for `isSecureMode`

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,3 @@
-import { apiKey, isSecureMode } from '~/logic'
-
 // This file contains various web utils, which interact with the Obsidian local REST API plugin.
 
 // Borrowed from `coddingtonbear/obsidian-web`.
@@ -18,7 +16,6 @@ export async function obsidianRequest(
     method: options.method?.toUpperCase(),
     mode: 'cors',
   }
-
   return fetch(
       `http${insecureMode ? '' : 's'}://127.0.0.1:${
           insecureMode ? '27123' : '27124'
@@ -34,21 +31,20 @@ export async function readDirectory() {
 }
 
 export async function readFromFile(filename: string) {
-  // TODO: Get secure mode working.
   return obsidianRequest(
     await browser.storage.local.get('apiKey').then((result) => result.apiKey),
     `/vault/${filename}`,
     { method: 'get' },
-    !isSecureMode.value,
+    await browser.storage.local.get('isSecureMode').then((result) => result.isSecureMode),
   )
 }
 
 export async function writeFile(filename: string, text: string) {
   return obsidianRequest(
-      await browser.storage.local.get('apiKey').then((result) => result.apiKey),
+    await browser.storage.local.get('apiKey').then((result) => result.apiKey),
       `/vault/${filename}`,
       { method: 'put', body: text },
-      !isSecureMode.value,
+      await browser.storage.local.get('isSecureMode').then((result) => result.isSecureMode),
   )
     .then((res, err) => {
       if (err)
